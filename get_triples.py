9313378f8aa75c204e0d5d3e36bf08cbee1f1b48
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2018-03-11 18:12:17 +0530

    fill in the blank type added

---
+++ get_triples.py
@@ -0,0 +1,67 @@
+# extract all triples and store them in npy format
+# import sys, os
+# sys.path.append(os.path.expanduser('~') + '/models/syntaxnet/')
+from svo_extractor import get_svo, preprocess
+from sentence_selector import SentenceSelection
+import spacy
+nlp = spacy.load("en")
+# def preprocess(sent):
+
+# 	tok_sent = [word.lower() for word in sent.split()]
+# 	return tok_sent
+
+
+class QuestionGenerator:
+
+
+	def get_text(self):
+		pass
+
+	def get_questions(self, sent):
+
+		# given a sentence generate questions
+		# return list of (question, answer)
+
+		# Step1 take a sentence and triple so all those triples will act as fill in the blanks
+		tok_sent, doc = preprocess(sent)
+		triples = get_svo(doc)	
+		# replace the token with a spl char like blank_0
+		# fill in the blanks type questions
+		questions = []
+
+		for i, svo in enumerate(triples):
+			for j, ele in enumerate(svo):
+				question = tok_sent[:]
+				for k, token in enumerate(tok_sent):
+					if token == ele:
+						question[k] = 'blank_0'
+						answer = ele
+						questions.append((question,answer))
+						break
+
+		return questions
+
+	def generate_questions(self, sentences):
+		questions = []
+		for sent in sentences:
+			questions.append(self.get_questions(sent))
+
+		return questions
+
+
+if __name__ == '__main__':
+
+	document = 'babur.txt'
+	qgen = QuestionGenerator()
+	ratio = 0.2
+	ss = SentenceSelection(ratio=ratio)
+	sentences = ss.prepare_sentences(document)
+	sents = sentences.values()[:]
+	# for sent in sentences.values():
+	# 	sents.append(sent)
+	# print sents
+	questions = qgen.generate_questions(sents)
+	#print questions
+	for question_set in questions:
+		for question in question_set:
+			print question,'\n'
