59b8548e7179e29bd7663c2f8d073515000c5096
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2018-03-22 21:07:26 +0530

    travis file added

--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,8 +1,9 @@
 
 import sys
-sys.path.insert(0, '../')
+sys.path.insert(0, '../ai_tutor')
 import spacy
 # other dependecies
+from ai_tutor import get_triples
 from ai_tutor.get_triples import QuestionGenerator
 from ai_tutor.svo_extractor import get_svo, preprocess
 from ai_tutor.sentence_selector import SentenceSelection
@@ -28,11 +29,11 @@ def preprocessing(filename):
 # test functions
 def test_extract_sentences():
 
-	filename = "test_files/babur.txt"
+	filename = "test/test_files/babur.txt"
 	sents = extract_best_sents(filename)
 	assert len(sents) > 0
 
 def test_preprocess():
 
-	filename = "test_files/babur.txt"
+	filename = "test/test_files/babur.txt"
 	preprocessing(filename)
