8a179aa93c6b342545b0a23a78c108cc50f48929
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2018-03-22 23:49:19 +0530

    modified installation files

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 pip install spacy
 pip install nltk
+apt-get install swig
+apt-get install libpulse-dev
 python -m spacy download en
 python -m nltk.downloader stopwords
 pip install textract
